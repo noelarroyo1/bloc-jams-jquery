f52c5fd13f8d3746847f56224edb081e3cf58068
Noel Arroyo <noel.arroyo1@gmail.com>
2019-09-08 21:45:11 -0400

    add more functionality to player-bar as well as next button

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,29 +1,33 @@
-$(document).ready(function(){
-  $('button#play-pause').click(function(){
+$(document).ready(function() {
+  $("button#play-pause").click(function() {
     player.playPause();
-    $(this).attr('playState', player.playState);
+    $(this).attr("playState", player.playState);
   });
 
-  $('button#next').click(function(){
-    if(player.playState !== 'playing'){return;}
+  $("button#next").click(function() {
+    if (player.playState !== "playing") {
+      return;
+    }
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
-    if(nextSongIndex >= album.songs.length){return;}
+    if (nextSongIndex >= album.songs.length) {
+      return;
+    }
 
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
 
-  $('#time-control input').on('input', function (event){
+  $("#time-control input").on("input", function(event) {
     player.skipTo(event.target.value);
   });
 
-  setInterval( () => {
+  setInterval(() => {
     const currentTime = player.getTime();
     const duration = player.getDuration();
-    const percent = (currentTime/duration) * 100;
-    $('#time-control .current-time').text(currentTime);
-    $('#time-control input').val(percent);
+    const percent = (currentTime / duration) * 100;
+    $("#time-control .current-time").text(currentTime);
+    $("#time-control input").val(percent);
   }, 1000);
 });
